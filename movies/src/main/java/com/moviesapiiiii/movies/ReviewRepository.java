5dd34f8cb5428330773680dc3950295dc2fb1d51
birukabza <birukabza@gmail.com>
2024-10-13 22:23:04 +0300

    initial commit

---
+++ movies/src/main/java/com/moviesapiiiii/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.moviesapiiiii.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+
+}
